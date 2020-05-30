5b9d435845ba328b054c259df513a53eeaf11fc5
Tom Milligan <tommilligan@users.noreply.github.com>
2020-05-30 21:58:25 +0100

    move lib functions back fom bin

--- src/bin/scron.rs
+++ src/bin/scron.rs
@@ -1,36 +1,9 @@
 use std::env;
-use std::fmt;
 use std::io::{self, BufRead};
 
 use anyhow::{Context, Result};
 
-const MINUTES_IN_HOUR: usize = 60;
-
-enum Day {
-    Today,
-    Tomorrow,
-}
-impl fmt::Display for Day {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Self::Today => "today",
-                Self::Tomorrow => "tomorrow",
-            }
-        )
-    }
-}
-
-enum Specifier {
-    Any,
-    Only(usize),
-}
-
-fn get_next_time(minute: Specifier, hour: Specifier, current_time: usize) -> (usize, Day) {
-    (0, Day::Today)
-}
+use simple_cron::{get_next_time, Specifier, MINUTES_IN_HOUR};
 
 fn parse_token(raw_token: &str) -> Result<Specifier> {
     Ok(match raw_token {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+use std::fmt;
+
+use anyhow::{Context, Result};
+
+pub const MINUTES_IN_HOUR: usize = 60;
+
+pub enum Day {
+    Today,
+    Tomorrow,
+}
+
+impl fmt::Display for Day {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Today => "today",
+                Self::Tomorrow => "tomorrow",
+            }
+        )
+    }
+}
+
+pub enum Specifier {
+    Any,
+    Only(usize),
+}
+
+pub fn get_next_time(minute: Specifier, hour: Specifier, current_time: usize) -> (usize, Day) {
+    (0, Day::Today)
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
